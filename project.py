5b29094c3e3ee2bb754a297f6d384a0ad0f40c94
jakobupton <jakejupton@gmail.com>
2023-12-08 00:31:20 -0800

    python gui and setup a little bit for solr

--- project.py
+++ project.py
@@ -1,4 +1,8 @@
+import tkinter as tk
+from tkinter import ttk
+import pysolr
 
+<<<<<<< HEAD
 import tabloo
 import pandas as pd
 import time
@@ -33,3 +37,69 @@ print(round((time.time() - start),3), 'seconds', flush='True')
 # Display
 tabloo.show(csv)
 
+=======
+# Creating a lil gui using tkinter
+window = tk.Tk()
+file = "intinerary.csv"
+window.geometry("800x600")
+window.title(f"Search through {file}")
+table = pysolr.Solr("http://localhost:8983/solr/itinerary", always_commit=True)
+# something like this to connect to the solr server, idk I haven't installed solr yet
+
+
+# Idk if this even works,  but it should get the column names from the schema
+# It should return them as a comma separated string, which is what we need
+columns = table.schema.get_field_names()
+# if not, lets try this, appending this string, found it on stackoverflow "select?q=*:*&wt=csv&rows=0&facet"
+
+
+# Create a treeview widget to display a table
+tree = ttk.Treeview(
+    window,  # Parent
+    columns,  # Columns
+    show="headings",  # Hide the default column
+)
+
+# Scrollbars because length might be long asl and width might also be long asl
+vsb = ttk.Scrollbar(root, orient="vertical", command=tree.yview)
+vsb.pack(side="right", fill="y")
+hsb = ttk.Scrollbar(root, orient="horizontal", command=tree.xview)
+hsb.pack(side="bottom", fill="x")
+tree.configure(yscrollcommand=vsb.set)
+
+# Define the column headings
+for col in columns:
+    tree.heading(col, text=col)
+tree.pack()
+
+
+# tree.insert("", "end", values=())
+
+
+window.mainloop()  # init the window innit
+
+
+# Hunters Code for reference, might be useful when we figure out solr
+
+# import tabloo
+# import pandas as pd
+
+# """
+
+# REDUCING DATA
+
+# """
+
+# # GET CSV
+# # Reducing the 27 column csv into 10 columns
+# csv = pd.read_csv("itineraries.csv", chunksize=5000, nrows=800000, usecols=['legId', 'flightDate', 'startingAirport', 'destinationAirport', 'travelDuration', 'isBasicEconomy', 'isRefundable', 'isNonStop', 'totalFare', 'seatsRemaining'])
+# csv = pd.concat(csv, ignore_index=True)
+
+
+# # Deleting flights with no empty seats
+# csv = csv[csv.seatsRemaining != 0]
+
+# # Only displaying the cheapest filght from A to B
+# # csv = csv.groupby(['startingAirport', 'destinationAirport']).min()
+# # csv['totalFare'] = csv.groupby(['startingAirport', 'destinationAirport'])['totalFare'].min()
+>>>>>>> b482aeb (python gui and setup a little bit for solr)
