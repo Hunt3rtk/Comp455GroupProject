52bd71504f443a04326d8bb3d7ecb9166fc6e55d
jakobupton <jakejupton@gmail.com>
2023-12-10 15:32:20 -0800

    connected postgresql container to frontend container

--- frontend/data.py
+++ frontend/data.py
@@ -1,7 +1,10 @@
 # importing flask
 from flask import Flask, render_template, request, url_for
+import time
+
 # importing postgres python api
-import psycopg
+import psycopg2
+
 # importing pandas module
 import pandas as pd
 
@@ -12,41 +15,47 @@ global cursor
 
 app = Flask(__name__)
 
+
 # connect to postgres database server
 def connect():
-
-    conn = psycopg.connect(
+    conn = psycopg2.connect(
         user="postgres",
         password="postgres",
-        host="localhost:5432",
+        host="db",
+        port="5432",
     )
     return conn.cursor()
 
+
 # create database for csv
 def csv_to_db(url, cursor):
-    cursor.execute("""
-        CREATE TABLE flights
-    """)
-    cursor.copy_from(url, 'flights', sep=',', columns=[
-        "legId",
-        "flightDate",
-        "startingAirport",
-        "destinationAirport",
-        "travelDuration",
-        "isBasicEconomy",
-        "isRefundable",
-        "isNonStop",
-        "totalFare",
-        "seatsRemaining",
-    ],)
+    cursor.execute("SELECT * FROM flights")
+    cursor.copy_from(
+        url,
+        "flights",
+        sep=",",
+        columns=[
+            "legId",
+            "flightDate",
+            "startingAirport",
+            "destinationAirport",
+            "travelDuration",
+            "isBasicEconomy",
+            "isRefundable",
+            "isNonStop",
+            "totalFare",
+            "seatsRemaining",
+        ],
+    )
     cursor.commit()
     return
 
 
-cursor = connect()
-url = '/frontend/itineraries.csv'
+url = "/frontend/itineraries.csv"
+test = connect()
 
-csv_to_db(url, cursor)
+
+csv_to_db(url, test)
 
 
 # route to html page - "table"
@@ -56,6 +65,7 @@ def table():
     cursor.execute("SELECT * FROM flights")
     data = cursor.fetchmany(100000)
     return render_template("table.html", tables=data, page=1, index=0, titles=[""])
+
 
 # route to update page to next table
 # @app.route("/next_page/<int:page>/<int:index>", methods=["POST"])
