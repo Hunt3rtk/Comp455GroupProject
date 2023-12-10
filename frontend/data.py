68d2826fabaa0a3ff3e9bd4fc9b638b8f2fdc4c9
Hunt3rtk <48702101+Hunt3rtk@users.noreply.github.com>
2023-12-10 13:22:44 -0800

    Connecting to db and copying csv

--- frontend/data.py
+++ frontend/data.py
@@ -1,22 +1,34 @@
 # importing flask
-from flask import Flask, render_template, url_for
-from sqlalchemy import create_engine, text
-
+from flask import Flask, render_template, request, url_for
+# importing postgres python api
+import psycopg2
 # importing pandas module
 import pandas as pd
 
 global page
 global index
-global dataArray
+global data
+global cursor
 
 app = Flask(__name__)
 
-# converting csv to html
-data = pd.read_csv(
-    "itineraries.csv",
-    chunksize=5000,
-    nrows=10000,
-    usecols=[
+# connect to postgres database server
+def connect():
+
+    conn = psycopg2.connect(
+        user="postgres",
+        password="postgres",
+        host="0.0.0.0",
+        port="5432"
+    )
+    return conn.cursor()
+
+# create database for csv
+def csv_to_db(url, cursor):
+    cursor.execute("""
+        CREATE TABLE flights
+    """)
+    cursor.copy_from(url, 'flights', sep=',', columns=[
         "legId",
         "flightDate",
         "startingAirport",
@@ -27,50 +39,42 @@ data = pd.read_csv(
         "isNonStop",
         "totalFare",
         "seatsRemaining",
-    ],
-)
-data = pd.concat(data, ignore_index=True)
+    ],)
+    cursor.commit()
+    return
 
-# splitting the csv int chunks for pages
-dataArray = [data[i : i + 100] for i in range(0, len(data), 50)]
-dataArray = [df.to_html() for df in dataArray]
 
-engine = create_engine("sqlite+pysqlite:///:memory:")
-data.to_sql("flights", engine)
+cursor = connect()
+url = '/frontend/itineraries.csv'
+
+csv_to_db(url, cursor)
 
 
 # route to html page - "table"
 @app.route("/")
 @app.route("/table")
 def table():
-    return render_template("table.html", tables=dataArray, page=1, index=0, titles=[""])
-
-
-# route to reset tables with filters applied
-@app.route("/table_reset")
-def page_reset():
-    print("yup", flush="True")
-    return
-
+    cursor.execute("SELECT * FROM flights")
+    data = cursor.fetchmany(100000)
+    return render_template("table.html", tables=data, page=1, index=0, titles=[""])
 
 # route to update page to next table
-@app.route("/next_page/<int:page>/<int:index>", methods=["POST"])
-def next_page(index, page):
-    ind = index + 1
-    pg = page + 1
-    return render_template(
-        "table.html", tables=dataArray, page=pg, index=ind, titles=[""]
-    )
+# @app.route("/next_page/<int:page>/<int:index>", methods=["POST"])
+# def next_page(index, page):
+#    ind = index + 1
+#    pg = page + 1
+#    return render_template(
+#        "table.html", tables=data, page=pg, index=ind, titles=[""]
+#    )
 
 
 @app.route("/select_column/")
 def select_column():
-    c = request.args.get("c")  # return the query as q
-    with engine.connect() as conn:
-        # let's select the column credit_history
-        # from the loan data table
-        result = conn.execute(text(f"SELECT {c} FROM flights"))
-    return render_template("table.html", tables=result, page=1, index=0, titles=[""])
+    q = request.args.get("q")  # return the query as q
+    cursor.execute("SELECT {q} FROM flights")
+    data = cursor.fetchmany(100000)
+
+    return render_template("table.html", tables=data, page=1, index=0, titles=[""])
 
 
 if __name__ == "__main__":
